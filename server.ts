330c8fe0a4046d39ec597db503ee6fd3374ff467
dikovm <dikovmax04@gmail.com>
2023-09-09 09:37:22 +0200

    proxying Issues

--- server.ts
+++ server.ts
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', router);
+app.set('trust proxy', true);
 
 let config_file = 'config.cfg';
 
